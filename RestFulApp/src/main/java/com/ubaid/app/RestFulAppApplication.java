bf6f590fd5ac8ce1f5387f33f8f378fc9c88b6ba
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-25 18:29:37 +0500

    initialize

---
+++ RestFulApp/src/main/java/com/ubaid/app/RestFulAppApplication.java
@@ -0,0 +1,15 @@
+package com.ubaid.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulAppApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(RestFulAppApplication.class, args);
+	}
+
+}
