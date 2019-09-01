2e7b6d9b24b4cae10351f26a075a4b514a80ca73
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-09-01 20:26:02 +0500

    spring-cloud-config-server add

---
+++ spring-cloud-config-server/src/main/java/com/ubaid/ms/SpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ubaid.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
