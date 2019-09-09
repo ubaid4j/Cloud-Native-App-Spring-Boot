a335f14013f97ca7fb6cff27d07845e468e222a0
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-09-09 22:37:06 +0500

    update

--- limits-service/src/main/java/com/ubaid/ms/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/ubaid/ms/LimitsServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class LimitsServiceApplication {
+public class LimitsServiceApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(LimitsServiceApplication.class, args);
 	}
 
