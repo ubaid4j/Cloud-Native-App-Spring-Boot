267cae170add28e18aa41f72962fe24e1d0c1183
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-02-07 13:04:24 +0500

    currency conversion add

---
+++ currency-conversion-client/src/main/java/com/ubaid/ms/currencyconversionclient/CurrencyConversionClientApplication.java
@@ -0,0 +1,13 @@
+package com.ubaid.ms.currencyconversionclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionClientApplication.class, args);
+	}
+
+}
