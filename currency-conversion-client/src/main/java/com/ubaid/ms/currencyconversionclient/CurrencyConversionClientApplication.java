274fd30a6bfcceacd1f19a36035105460dcd44aa
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-02-07 13:11:30 +0500

    client add

--- currency-conversion-client/src/main/java/com/ubaid/ms/currencyconversionclient/CurrencyConversionClientApplication.java
+++ currency-conversion-client/src/main/java/com/ubaid/ms/currencyconversionclient/CurrencyConversionClientApplication.java
@@ -2,9 +2,14 @@ package com.ubaid.ms.currencyconversionclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class CurrencyConversionClientApplication {
+@EnableFeignClients(basePackages = {"com.ubaid.ms"})
+@EnableDiscoveryClient
+public class CurrencyConversionClientApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionClientApplication.class, args);
