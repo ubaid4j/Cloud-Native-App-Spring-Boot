a5fe383effa43d37aeaeee24caf9d2c7214650f3
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-08-23 12:18:59 +0500

    - https://github.com/UbaidurRehman1/SpringServices_Rest_Micro/issues/7

--- limits-service/src/main/java/com/ubaid/ms/config/Config.java
+++ limits-service/src/main/java/com/ubaid/ms/config/Config.java
@@ -4,44 +4,42 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * 
- * 
  * and define the prefix
- * same as in the application.properties file 
- * 
- * @author UbaidurRehman
+ * same as in the application.properties file
  *
+ * @author UbaidurRehman
  */
 
 @ConfigurationProperties(prefix = "limits-service")
 @Component
-public class Config
-{
-	
-	private int min;
-	private int max;
+public class Config {
 
-	public int getMin() {
-		return min;
-	}
-	public void setMin(int min) {
-		this.min = min;
-	}
+    private int min;
+    private int max;
 
-	public int getMax() {
-		return max;
-	}
-	public void setMax(int max) {
-		this.max = max;
-	}
-	public Config(int min, int max) {
-		this.min = min;
-		this.max = max;
-	}
-		
-	public Config()
-	{
+    public int getMin() {
+        return min;
+    }
 
-	}
-	
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public Config(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public Config() {
+
+    }
+
 }
--- limits-service/src/main/java/com/ubaid/ms/entity/LimitsConfig.java
+++ limits-service/src/main/java/com/ubaid/ms/entity/LimitsConfig.java
@@ -1,35 +1,33 @@
 package com.ubaid.ms.entity;
 
-public class LimitsConfig
-{
-	private int max;
-	private int min;
-	
-	public LimitsConfig()
-	{
-		
-	}
 
-	public int getMax() {
-		return max;
-	}
+public class LimitsConfig {
+    private int max;
+    private int min;
 
-	public void setMax(int max) {
-		this.max = max;
-	}
+    public LimitsConfig() {
 
-	public int getMin() {
-		return min;
-	}
+    }
 
-	public void setMin(int min) {
-		this.min = min;
-	}
+    public int getMax() {
+        return max;
+    }
 
-	public LimitsConfig(int max, int min)
-	{
-		this.max = max;
-		this.min = min;
-	}
-	
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public LimitsConfig(int max, int min) {
+        this.max = max;
+        this.min = min;
+    }
+
 }
