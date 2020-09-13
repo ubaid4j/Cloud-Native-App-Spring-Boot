174ee26383073e710a8b12ddac79c558017775fa
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-09-13 12:22:57 +0500

    - https://github.com/UbaidurRehman1/SpringServices_Rest_Micro/issues/42

--- currency-conversion-api/src/main/java/com/ubaid/ms/currencyconversionclient/feignProxy/LimitProxy.java
+++ currency-conversion-api/src/main/java/com/ubaid/ms/currencyconversionclient/feignProxy/LimitProxy.java
@@ -1,15 +1,13 @@
 package com.ubaid.ms.currencyconversionclient.feignProxy;
 
+import com.ubaid.ms.currencyconversionclient.entity.Limit;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.ubaid.ms.currencyconversionclient.entity.Limit;
-
 @FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "limits-service")
-public interface LimitProxy
-{
-	@GetMapping("/limits-service/limits")
-	public Limit getLimitConfig();
+public interface LimitProxy {
+    @GetMapping("/limits-service/limits")
+    Limit getLimitConfig();
 }
