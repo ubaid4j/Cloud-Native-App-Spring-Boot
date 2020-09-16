57eaa2d9f09fc9ef3e8b172f0e5ca362a8beb3f0
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-09-16 08:37:33 +0500

    - https://github.com/UbaidurRehman1/SpringServices_Rest_Micro/issues/62

---
+++ currency-conversion-api/src/main/java/com/ubaid/ms/currencyconversionapi/aop/Logging.java
@@ -0,0 +1,32 @@
+package com.ubaid.ms.currencyconversionapi.aop;
+
+import com.ubaid.ms.currencyconversionapi.exception.UnknownException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class Logging extends TargetMethods {
+
+    @Around("convertCurrency()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result;
+        try {
+            Object[] args = joinPoint.getArgs();
+            String fromCurrency = (String) args[0];
+            String toCurrency = (String) args[1];
+            Integer quantity = (Integer) args[2];
+            log.info("Converting {} {} to {}", quantity, fromCurrency, toCurrency);
+            result = joinPoint.proceed();
+
+        } catch (Exception exp) {
+            log.error("Exception Occurred: Message: [{}]", exp.getMessage());
+            throw new UnknownException(exp.getMessage(), exp);
+        }
+        return result;
+    }
+}
---
+++ currency-conversion-api/src/main/java/com/ubaid/ms/currencyconversionapi/exception/UnknownException.java
@@ -0,0 +1,11 @@
+package com.ubaid.ms.currencyconversionapi.exception;
+
+public class UnknownException extends RuntimeException {
+    public UnknownException(String message) {
+        super(message);
+    }
+
+    public UnknownException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
