7240aacfc1f77fb728d0039fd67279d948240737
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-10-04 18:15:18 +0500

    - change Object to DTO in rest api logging

--- rest-api/src/main/java/com/ubaid/ms/restapi/aop/Logging.java
+++ rest-api/src/main/java/com/ubaid/ms/restapi/aop/Logging.java
@@ -1,5 +1,6 @@
 package com.ubaid.ms.restapi.aop;
 
+import com.ubaid.ms.ccdto.ExchangeValueDTO;
 import com.ubaid.ms.module.ccexception.CCException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -13,15 +14,16 @@ import org.springframework.stereotype.Component;
 public class Logging extends TargetMethods {
 
     @Around("convertCurrency()")
-    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        Object result;
+    public ExchangeValueDTO doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        ExchangeValueDTO result = null;
         try {
             Object[] args = joinPoint.getArgs();
             String fromCurrency = (String) args[0];
             String toCurrency = (String) args[1];
             Integer quantity = (Integer) args[2];
             log.debug("Converting {} {} to {}", quantity, fromCurrency, toCurrency);
-            result = joinPoint.proceed();
+            result = (ExchangeValueDTO) joinPoint.proceed();
+            log.debug("The Converted Currency is: {}", result);
 
         } catch (Exception exp) {
             log.error("Exception Occurred: Message: [{}]", exp.getMessage());
