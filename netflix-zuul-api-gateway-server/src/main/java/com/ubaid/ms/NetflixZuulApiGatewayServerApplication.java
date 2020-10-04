2bf4cbc6d3cc181f61072f1531267aee2d100e11
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-10-04 18:28:18 +0500

    - #140

--- netflix-zuul-api-gateway-server/src/main/java/com/ubaid/ms/NetflixZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/com/ubaid/ms/NetflixZuulApiGatewayServerApplication.java
@@ -1,4 +1,4 @@
-package com.ubaid.ms.gatewayserver;
+package com.ubaid.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
