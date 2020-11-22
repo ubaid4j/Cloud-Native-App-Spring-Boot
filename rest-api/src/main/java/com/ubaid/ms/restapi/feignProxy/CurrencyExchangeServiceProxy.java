9161512e580d1deef17a022366333dea6377ae95
Ubaid ur Rehman <ubaidshan007@gmail.com>
2020-11-22 11:55:45 +0500

    Revert "- update Spring boot version to 2.4.x update-#209"

--- rest-api/src/main/java/com/ubaid/ms/restapi/feignProxy/CurrencyExchangeServiceProxy.java
+++ rest-api/src/main/java/com/ubaid/ms/restapi/feignProxy/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,13 @@
 package com.ubaid.ms.restapi.feignProxy;
 
 import com.ubaid.ms.ccdto.ExchangeValueDTO;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "spring-cloud-api-gateway-server")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
