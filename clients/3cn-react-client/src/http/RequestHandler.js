62e65da114956a600e7625ccf77d8971dc7f9b2e
UbaidurReham1 <ubaidshan007@gmail.com>
2020-12-09 13:59:16 +0500

    - deploy react client (#224)

--- clients/3cn-react-client/src/http/RequestHandler.js
+++ clients/3cn-react-client/src/http/RequestHandler.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
+
+const env = process.env.NODE_ENV === 'development';
+let baseURL;
+if (env) {
+    baseURL = 'http://localhost:5200/api/';
+} else {
+    baseURL = 'https://b5ada40c55b9.ngrok.io/api/';
+}
+
 const RequestHandler = axios.create({
-    baseURL: 'http://localhost:5200/api/',
+    baseURL: baseURL,
     headers: {'Access-Control-Allow-Origin': '*'}
 });
 export default RequestHandler;
