96db9f372f208fa37a93d627bfd0aee7ba60af7c
UbaidurReham1 <ubaidshan007@gmail.com>
2020-12-10 23:56:29 +0500

    - add feign client to get token (#252)

--- rest-api/src/main/java/com/ubaid/ms/restapi/service/AuthService.java
+++ rest-api/src/main/java/com/ubaid/ms/restapi/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.ubaid.ms.restapi.service;
 
+import java.util.Map;
+
 /**
  * Return a bearer token in full format
  * @author ubaid
  */
 public interface AuthService {
     String getBearerToken();
+    Map<String, ?> login(String username, String password);
 }
