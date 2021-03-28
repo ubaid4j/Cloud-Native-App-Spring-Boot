c3c8213a11d38fae472ea18e4daf942f880e1dbd
UbaidurReham1 <ubaidshan007@gmail.com>
2021-03-28 20:07:47 +0500

    - login functionality (#288) - add docker file and docker compose for react client

--- clients/3cn-react-client/src/keycloak.js
+++ clients/3cn-react-client/src/keycloak.js
@@ -1,5 +1,4 @@
 import Keycloak from 'keycloak-js';
-
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const kc = new Keycloak();
