a3fe42029de1242466b06547f947067ee6de3486
UbaidurReham1 <ubaidshan007@gmail.com>
2021-04-11 15:37:10 +0500

    Upgrade to Java 16 (#308) - enhance install-maven-jdk16.sh

--- install-maven-jdk16.sh
+++ install-maven-jdk16.sh
@@ -8,7 +8,13 @@ sudo rm -rf /opt/maven-3.8.1
 sudo tar -xvf /tmp/apache-maven-3.8.1-bin.tar.gz -C /opt
 sudo rm -rf /opt/maven
 sudo ln -s /opt/apache-maven-3.8.1 /opt/maven
-sudo sh -c "echo export JAVA_HOME=/opt/java  export MAVEN_HOME=/opt/maven  export M2_HOME=/opt/maven export PATH=${M2_HOME}/bin:${PATH} >> /etc/profile.d/maven.sh"
+sudo rm -rf /etc/profile.d/maven.sh
+sudo sh -c "echo export JAVA_HOME=/opt/java  export MAVEN_HOME=/opt/maven  export M2_HOME=/opt/maven export PATH=/opt/maven/bin:${PATH} >> /etc/profile.d/maven.sh"
 sudo chmod +x /etc/profile.d/maven.sh
+sudo rm -rf /usr/bin/mvn
+sudo rm -rf /usr/bin/java
+sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
+sudo ln -s /opt/java/bin/java /usr/bin/java
+export JAVA_HOME=/opt/java
 source /etc/profile.d/maven.sh
 mvn --version
