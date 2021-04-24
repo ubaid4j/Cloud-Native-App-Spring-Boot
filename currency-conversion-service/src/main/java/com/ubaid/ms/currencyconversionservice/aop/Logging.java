7f8168f551df3a59960cc0e1ecd4fb1165c55794
UbaidurReham1 <ubaidshan007@gmail.com>
2021-04-25 02:39:06 +0500

    enhance AOP logging (#300)

--- currency-conversion-service/src/main/java/com/ubaid/ms/currencyconversionservice/aop/Logging.java
+++ currency-conversion-service/src/main/java/com/ubaid/ms/currencyconversionservice/aop/Logging.java
@@ -15,19 +15,38 @@ import org.springframework.stereotype.Component;
 public class Logging extends TargetMethods {
 
     @Around("convert()")
-    public ConvertedCurrency doLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        ConvertedCurrency result;
+    public ConvertedCurrency doLog(ProceedingJoinPoint pjp) throws Throwable {
+        logMethodStart(pjp);
+        long start = System.currentTimeMillis();
         try {
-            Object[] args = joinPoint.getArgs();
+            Object[] args = pjp.getArgs();
             Double quantity = (Double) args[0];
             Double conversionRate = (Double) args[1];
-            result = (ConvertedCurrency) joinPoint.proceed();
-            log.debug("Currency Value: {} * Conversion Rate: {} = Converted Currency: {}", quantity, conversionRate, result.getConvertedCurrency());
-
+            ConvertedCurrency convertedCurrency = (ConvertedCurrency) pjp.proceed();
+            log.debug("Currency Value: {} * Conversion Rate: {} = Converted Currency: {}", quantity, conversionRate, convertedCurrency.getConvertedCurrency());
+            long elapsedTime = System.currentTimeMillis() - start;
+            logMethodExit(pjp, elapsedTime);
+            return convertedCurrency;
         } catch (Exception exp) {
             log.error("Exception Occurred: Message: [{}]", exp.getMessage());
-            throw new CCException("Unknown Exception");
+            throw new CCException(exp.getMessage(), exp);
         }
-        return result;
     }
+
+    private void logMethodStart(ProceedingJoinPoint pjp) {
+        String packageName = pjp.getSignature().getDeclaringTypeName();
+        String methodName = pjp.getSignature().getName();
+        if(!pjp.getSignature().getName().equals("initBinder")) {
+            log.info("Entering method [" + packageName + "." + methodName +  "]");
+        }
+    }
+
+    private void logMethodExit(ProceedingJoinPoint pjp, long elapsedTime) {
+        String methodName = pjp.getSignature().getName();
+        String packageName = pjp.getSignature().getDeclaringTypeName();
+        if(!methodName.equals("initBinder")) {
+            log.info("Exiting method [" + packageName + "." + methodName + "]; exec time (ms): " + elapsedTime);
+        }
+    }
+
 }
