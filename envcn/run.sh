34d5038898c84d72616e607e55cc37623fe12713
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-05-18 13:32:40 +0500

    Fix audit db not created automatically (gh-370)

--- envcn/run.sh
+++ envcn/run.sh
@@ -1,1 +1,11 @@
+#Cleaing Database so, that, we can run  /docker-entrypoint-initdb.d/init.sql (gh-370)
+#If there is data inside the database then /docker-entrypoint-initdb.d/init.sql not running
+docker container stop cn-db;
+docker container rm cn-db;
+docker volume rm envcn_cloud-native-volume;
+
+docker container stop auth-db;
+docker container rm auth-db;
+docker volume rm envcn_auth-db-volume;
+
 docker-compose up --build
