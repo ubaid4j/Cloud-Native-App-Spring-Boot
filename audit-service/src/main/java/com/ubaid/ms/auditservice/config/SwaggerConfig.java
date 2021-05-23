14473adf3a3742d2f2086e1e99f8b272bd31019a
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-05-23 16:23:51 +0500

    api docx (gh-342) - add audit-service api docx

--- audit-service/src/main/java/com/ubaid/ms/auditservice/config/SwaggerConfig.java
+++ audit-service/src/main/java/com/ubaid/ms/auditservice/config/SwaggerConfig.java
@@ -4,13 +4,16 @@ import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.security.Principal;
 import java.util.List;
 
 
@@ -20,15 +23,19 @@ import java.util.List;
 public class SwaggerConfig {
 
     public static final String AUTHORIZATION = "Authorization";
+    public static final String AUDIT = "AUDIT";
 
     @Bean
     public Docket swaggerSpringfoxDocket() {
         return new Docket(DocumentationType.OAS_30)
+                .tags(new Tag(AUDIT, String.format("REST API fro %s", AUDIT)))
+                .ignoredParameterTypes(Principal.class)
                 .apiInfo(apiInfo())
                 .securityContexts(Lists.newArrayList(securityContext()))
                 .securitySchemes(Lists.newArrayList(bearerToken()))
                 .useDefaultResponseMessages(false)
                 .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .build();
     }
 
@@ -57,9 +64,9 @@ public class SwaggerConfig {
      * @return ApiInfo
      */
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("Audit API").description("CRUD Operations on Audit")
+        return new ApiInfoBuilder().title("Audit Logs API").description("CRUD Operations on Audit Logs")
                 .contact(new Contact("Ubaid ur Rehman", "https://www.linkedin.com/in/ubaid-ur-rehman-5a0118119/", "urehman.bese16seecs@seecs.edu.pk"))
-                .license("Open Source")
+                .license("MIT")
                 .licenseUrl("https://github.com/UbaidurRehman1/Cloud-Native-App-Spring-Boot/blob/master/LICENSE")
                 .version("1.0.0")
                 .build();
--- audit-service/src/main/java/com/ubaid/ms/auditservice/controller/AuditController.java
+++ audit-service/src/main/java/com/ubaid/ms/auditservice/controller/AuditController.java
@@ -1,14 +1,22 @@
 package com.ubaid.ms.auditservice.controller;
 
+import com.ubaid.ms.auditservice.config.SwaggerConfig;
 import com.ubaid.ms.auditservice.entity.Audit;
 import com.ubaid.ms.auditservice.service.AuditService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import org.apache.http.HttpStatus;
 
 import java.security.Principal;
 
+@Api(tags = SwaggerConfig.AUDIT)
 @RestController
 @RequestMapping("audit-service")
 @RequiredArgsConstructor
@@ -16,34 +24,64 @@ public class AuditController {
 
     private final AuditService auditService;
 
-    @GetMapping("/{id}")
+    @ApiOperation(value = "Get Audit log by Id", response = Audit.class)
+    @ApiResponses({
+            @ApiResponse(code = HttpStatus.SC_OK, message = "An Audit Log fetched successfully"),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = "You are not authorized to get Audit Log")
+    })
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Audit> findById(@PathVariable Long id) {
         return auditService.findById(id);
     }
 
-    @GetMapping("/user")
+    @ApiOperation(value = "Get All Audit Logs by User Id", response = Audit.class, responseContainer = "List")
+    @ApiResponses({
+            @ApiResponse(code = HttpStatus.SC_OK, message = "All audit logs of current user fetched successfully"),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = "You are unauthorized to get audit logs")
+    })
+    @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Audit> findAllByUserId(Principal principal) {
         return auditService
                 .findAllByUserUUID(principal);
     }
 
-    @GetMapping
+    @ApiOperation(value = "Get All available Logs", response = Audit.class, responseContainer = "List")
+    @ApiResponses({
+            @ApiResponse(code = HttpStatus.SC_OK, message = "All available audit logs fetched successfully"),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = "You are unauthorized to get all audit logs")
+    })
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Audit> findAll() {
         return auditService.findAll();
     }
 
 
-    @PostMapping
+    @ApiOperation(value = "Save Audit Log", response = Audit.class)
+    @ApiResponses({
+            @ApiResponse(code = HttpStatus.SC_OK, message = "Audit Log saved successfully"),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = "You are unauthorized to save Audit Log")
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Audit> save(Principal principal, @RequestBody Audit audit) {
         return auditService.save(principal, audit);
     }
 
-    @PutMapping
+    @ApiOperation(value = "Update Audit Log", response = Audit.class)
+    @ApiResponses({
+            @ApiResponse(code = HttpStatus.SC_OK, message = "Audit Log updated successfully"),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = "You are unauthorized to update Audit Log")
+    })
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Audit> update(Principal principal, @RequestBody Audit audit) {
         return auditService.update(principal, audit);
     }
 
-    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete Audit Log")
+    @ApiResponses({
+            @ApiResponse(code = HttpStatus.SC_OK, message = "Audit Log deleted successfully"),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = "You are unauthorized to delete Audit Log")
+    })
+    @DeleteMapping(value = "/{id}")
     public Mono<Void> delete(@PathVariable Long id) {
         return auditService.delete(id);
     }
