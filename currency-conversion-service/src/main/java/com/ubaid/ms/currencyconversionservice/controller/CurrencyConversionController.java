51dd1c65f8d032e2af4395d5199b5d2318aac369
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-05-24 10:51:43 +0500

    api docx (gh-342) - add exchange and convert api docx

--- currency-conversion-service/src/main/java/com/ubaid/ms/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/ubaid/ms/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,9 +1,16 @@
 package com.ubaid.ms.currencyconversionservice.controller;
 
 import com.ubaid.ms.ccdto.ConvertedCurrency;
+import com.ubaid.ms.currencyconversionservice.config.SwaggerConfig;
 import com.ubaid.ms.currencyconversionservice.service.CurrencyConversionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
 
+@Api(tags = SwaggerConfig.CONVERSION)
 @RestController
 @RequestMapping("currency-conversion")
 @Slf4j
@@ -20,8 +28,13 @@ public class CurrencyConversionController {
 
     private final CurrencyConversionService currencyConversionService;
 
+    @ApiOperation(value = "Convert given currency to another currency using given conversion rate", response = ConvertedCurrency.class)
+    @ApiResponses({
+            @ApiResponse(code = HttpStatus.SC_OK, message = "Currency converted successfully"),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = "You are not authorized to convert currency")
+    })
     @PreAuthorize("hasAuthority('SCOPE_currency-conversion')")
-    @GetMapping("/currency/{currency}/rate/{conversion-rate}")
+    @GetMapping(value = "/currency/{currency}/rate/{conversion-rate}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ConvertedCurrency getCurrencyConversion(
             @PathVariable("currency") Double currency,
             @PathVariable("conversion-rate") Double conversionRate) {
