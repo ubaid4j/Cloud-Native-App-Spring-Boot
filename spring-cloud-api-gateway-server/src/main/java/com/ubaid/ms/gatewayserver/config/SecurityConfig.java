47f46079937090bb9fecbd9bec6e37375ae57cef
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-05-29 23:32:06 +0500

    increase timeouts of JWT decoder rest operations (gh-381)

--- spring-cloud-api-gateway-server/src/main/java/com/ubaid/ms/gatewayserver/config/SecurityConfig.java
+++ spring-cloud-api-gateway-server/src/main/java/com/ubaid/ms/gatewayserver/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.ubaid.ms.gatewayserver.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.web.client.RestOperations;
+
+import java.time.Duration;
 
 /**
  * <pre>
@@ -23,7 +31,11 @@ import org.springframework.security.web.server.util.matcher.ServerWebExchangeMat
 @Configuration
 @EnableWebFluxSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
+@Slf4j
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
     private final static String[] DOWN_STREAM_SERVICES_PATHS = {"/user-service/oauth/token", "/convert/**", "/currency-conversion/**", "/currency-exchange/**", "/country/code/**", "/config/limits/**", "/audit-service/**"};
     private final static String[] OAUTH_TOKEN_PATHS = {"/user-service/oauth/token"};
@@ -78,5 +90,15 @@ public class SecurityConfig {
 
     public NegatedServerWebExchangeMatcher getNegatedMatcherForSwagger() {
         return new NegatedServerWebExchangeMatcher(exchange -> ServerWebExchangeMatchers.pathMatchers(SWAGGER_URLS).matches(exchange));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RestTemplateBuilder builder) {
+        RestOperations rest = builder
+                .setConnectTimeout(Duration.ofMinutes(3))
+                .setReadTimeout(Duration.ofMinutes(3))
+                .build();
+        log.info("Setting Connect Time out and Read Time out to 180 seconds for Rest Operations of JWT Decoder");
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).restOperations(rest).build();
     }
 }
