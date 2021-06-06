094d18268035662de3528cbda9c19f66bb149d0b
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-06-06 23:01:29 +0500

    two stage build docker (gh-378)

---
+++ envcn/run-multi-stage.sh
@@ -0,0 +1,13 @@
+#Cleaing Database so, that, we can run  /docker-entrypoint-initdb.d/init.sql (gh-370)
+#If there is data inside the database then /docker-entrypoint-initdb.d/init.sql not running
+docker container stop cn-db;
+docker container rm cn-db;
+docker volume rm envcn_cloud-native-volume;
+
+#There is a random error (gh-282) when starting auth-server.
+#To remove the possibility of gh-282, we are removing auth-db data.
+docker container stop auth-db;
+docker container rm auth-db;
+docker volume rm envcn_auth-db-volume;
+
+docker-compose -f docker-compose-multi-stage-build.yml up --build
---
+++ run-multi-stage.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose-multi-stage-build.yml up --build;
