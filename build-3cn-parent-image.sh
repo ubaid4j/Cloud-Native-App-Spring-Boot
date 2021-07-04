a067c3346efb23e38af2cb82c82d910415416ceb
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-07-04 17:19:19 +0500

    change dev to 0.0.2-SNAPSHOT (gh-431)

--- build-3cn-parent-image.sh
+++ build-3cn-parent-image.sh
@@ -1,2 +1,2 @@
-docker build -t 3cn-parent:0.0.1 -f Dockerfile.3cn-parent-init .
-docker tag 3cn-parent:0.0.1 ubaidurehman/3cn-parent:0.0.1
+docker build -t 3cn-parent:0.0.2-SNAPSHOT -f Dockerfile.3cn-parent-init .
+docker tag 3cn-parent:0.0.2-SNAPSHOT ubaidurehman/3cn-parent:0.0.2-SNAPSHOT
--- update-3cn-parent-image.sh
+++ update-3cn-parent-image.sh
@@ -1,2 +1,2 @@
-docker build -t 3cn-parent:0.0.1 -f Dockerfile.update-3cn-parent .
-docker tag 3cn-parent:0.0.1 ubaidurehman/3cn-parent:0.0.1
+docker build -t 3cn-parent:0.0.2-SNAPSHOT -f Dockerfile.update-3cn-parent .
+docker tag 3cn-parent:0.0.2-SNAPSHOT ubaidurehman/3cn-parent:0.0.2-SNAPSHOT
