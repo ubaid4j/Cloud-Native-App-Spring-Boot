9bb87ea6c097b005a4d505516db45e531fbd79d5
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-07-04 14:56:19 +0500

    change docker images builds to point 0.0.1 (gh-431)

--- build-3cn-parent-image.sh
+++ build-3cn-parent-image.sh
@@ -1,2 +1,2 @@
-docker build -t 3cn-parent:0.0.1-SNAPSHOT -f Dockerfile.3cn-parent-init .
-docker tag 3cn-parent:0.0.1-SNAPSHOT ubaidurehman/3cn-parent:0.0.1-SNAPSHOT
+docker build -t 3cn-parent:0.0.1 -f Dockerfile.3cn-parent-init .
+docker tag 3cn-parent:0.0.1 ubaidurehman/3cn-parent:0.0.1
--- update-3cn-parent-image.sh
+++ update-3cn-parent-image.sh
@@ -1,2 +1,2 @@
-docker build -t 3cn-parent:0.0.1-SNAPSHOT -f Dockerfile.update-3cn-parent .
-docker tag 3cn-parent:0.0.1-SNAPSHOT ubaidurehman/3cn-parent:0.0.1-SNAPSHOT
+docker build -t 3cn-parent:0.0.1 -f Dockerfile.update-3cn-parent .
+docker tag 3cn-parent:0.0.1 ubaidurehman/3cn-parent:0.0.1
