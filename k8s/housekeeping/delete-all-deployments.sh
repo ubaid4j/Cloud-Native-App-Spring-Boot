9c9b59cd0ead53fa3553f4de26663d0457101802
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-07-22 17:57:55 +0500

    make enhancement in housekeeping scripts (gh-451)

--- k8s/housekeeping/delete-all-deployments.sh
+++ k8s/housekeeping/delete-all-deployments.sh
@@ -1,13 +1,6 @@
-kubectl delete -n default deployment elasticsearch
-kubectl delete -n default deployment logstash
-kubectl delete -n default deployment kibana
-
-kubectl delete -n default deployment rabbitmq-deployment
-kubectl delete -n default deployment zipkin-deployment
-
-kubectl delete -n default deployment auth-db
-kubectl delete -n default deployment auth-server-deployment
-
-kubectl delete -n default deployment mysql
-kubectl delete -n default deployment config-deployment
-kubectl delete -n default deployment discovery
+#!/bin/bash
+declare -a deployments=("api-gateway" "audit" "config" "country" "currency-conversion" "currency-exchange" "discovery" "math" "user" "fe" "elasticsearch" "logstash" "kibana" "rabbit-mq" "zipkin" "auth-db" "auth-server" "app-db")
+for deployment in "${deployments[@]}"
+do
+  kubectl delete -n default deployment "$deployment"
+done
