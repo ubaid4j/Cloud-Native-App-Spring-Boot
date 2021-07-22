331a645f112d0a4b3a6d8c83024559abaaace8c6
UbaidurRehman1 <ubaidshan007@gmail.com>
2021-07-23 01:16:37 +0500

    add index endpoint to point swagger ui in api-gateway (gh-450)

---
+++ api-gateway/src/main/java/com/ubaid/ms/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.ubaid.ms.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@RestController(value = "/")
+public class IndexController {
+
+    @GetMapping(value = "/")
+    public Mono<Void> index(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.PERMANENT_REDIRECT);
+        response.getHeaders().setLocation(URI.create("/swagger-ui/index.html"));
+        return response.setComplete();
+    }
+}
