532866410f682d38536638b4806aaa11ef929abc
UbaidurRehman1 <ubaidshan007@gmail.com>
2022-01-30 22:01:15 +0500

    update config server and discovery server to fix spring fox exception with spring boot 2.6.2 (gh-512)

--- discovery/src/main/java/com/ubaid/ms/discovery/DiscoveryApp.java
+++ discovery/src/main/java/com/ubaid/ms/discovery/DiscoveryApp.java
@@ -1,12 +1,59 @@
 package com.ubaid.ms.discovery;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApp {
 
     public static void main(String[] args) { SpringApplication.run(DiscoveryApp.class, args); }
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider
+                    || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(
+                List<T> mappings) {
+                List<T> copy = mappings.stream()
+                    .filter(mapping -> mapping.getPatternParser() == null)
+                    .toList();
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    assert field != null;
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+
 }
