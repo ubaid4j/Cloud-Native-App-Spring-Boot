778835ce3eb38ae82f99393c71224001223e591c
ubaid4j <ubaidshan007@gmail.com>
2024-01-27 21:00:25 +0500

    - social app project

--- api-gateway/src/main/webapp/app/app.component.ts
+++ api-gateway/src/main/webapp/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'That will be some thing';
+  title = 'The Social Platform';
 }
