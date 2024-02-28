7215c4ff8cce8db03aa2aa1f2efcb8d1966db217
ubaid4j <ubaidshan007@gmail.com>
2024-02-28 10:47:21 +0500

    - enhance code

--- deployment/docker/api-gateway/health-check.sh
+++ deployment/docker/api-gateway/health-check.sh
@@ -3,7 +3,7 @@ exec 3<>/dev/tcp/localhost/80
 
 echo -e "GET /management/health HTTP/1.1\nhost: localhost:80\n" >&3
 
-timeout --preserve-status 180 cat <&3 | grep -m 1 status | grep -m 1 UP
+timeout --preserve-status 3 cat <&3 | grep -m 1 status | grep -m 1 UP
 ERROR=$?
 
 exec 3<&-
